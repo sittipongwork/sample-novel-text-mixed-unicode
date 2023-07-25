cabb47feca0f69cf6f061d4ee2233ff9d25ee672
sittipongwork <sittipongwork@gmail.com>
2023-07-25 14:44:00 +0700

    add readme information

--- src/pages/api/convert-text.tsx
+++ src/pages/api/convert-text.tsx
@@ -4,13 +4,12 @@ import { rawText, sampleCode } from '../../lib/sample-text';
 export default async function handler (req: NextApiRequest,
   res: NextApiResponse) {
     let result: string[] = []
-    var promises = rawText.map(function(_v) {
+    rawText.map(function(_v) {
       Object.entries(sampleCode).map(([k, v]) => {
         _v = _v.replaceAll(k, v)
       })
       return result.push(_v)
     });
-    await Promise.all(promises)
     return res.json({
       data: result
     })
